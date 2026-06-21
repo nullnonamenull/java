69e4a7833284b3f17f53674525ab4b9ca8825ba1
Piotras <poleszak@protonmail.com>
2026-06-21 11:35:39 +0200

    feat(doc-forge): initial Spring Boot project with Pandoc conversion endpoint

---
+++ spring_boot/doc-forge/src/main/java/com/noname/docforge/DocForgeApplication.java
@@ -0,0 +1,13 @@
+package com.noname.docforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocForgeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DocForgeApplication.class, args);
+    }
+
+}
---
+++ spring_boot/doc-forge/src/test/java/com/noname/docforge/DocForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname.docforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
